b2fb26b45de4b3a03d1d9d0d760f6aede056e1cf
JdeGuise <degui1jr@cmich.edu>
2016-01-28 17:39:02 -0500

    Updated class names

---
+++ CPS 410 Group Project/src/ManagerMain.java
@@ -0,0 +1,13 @@
+
+public class ManagerMain {
+
+	//contains financial formulas for calculating estimates,
+	//and CRUD operations to the database
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
